4362e4a7583c950078d902fbcc8a73e56fe93ea0
Rainer Stropek <rainer@timecockpit.com>
2025-09-19 10:25:59 +0200

    Saved in TypeScript Playground at 2025-09-19T08:25:59.295Z

--- robot/index.ts
+++ robot/index.ts
@@ -2,23 +2,22 @@ function setup() {
     createCanvas(5000,5000)
     background("lightgrey")
 
-
+    // Head
     fill("grey")
     rect(100,200,400,400)
 
-
+    // Eyes
     fill("black")
-    circle(180,330,80)
+    circle(180,330,80) // Left eye
+    circle(420,330,80) // Right eye
 
-    circle(420,330,80)
-
-
+    // Antenne
     strokeWeight(2)
     line(300,100,300,200)
 
     fill("white")
     circle(300,100,50)
 
-
+    // Mund
     rect(200,450,200,50)
 }
