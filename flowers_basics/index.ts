516f2be8c196bef38843dd5c9b3ec982978efb82
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 08:33:54 +0200

    Saved in TypeScript Playground at 2025-10-10T06:33:53.696Z

---
+++ flowers_basics/index.ts
@@ -0,0 +1,15 @@
+// <<< ADD CONSTANTS HERE (if you need them)
+
+function setup() {
+  angleMode(DEGREES)
+  createCanvas(1000,1000)
+  background("lightblue")
+
+  strokeWeight(10)
+  fill("lightblue")
+  arc(300,300,100,100,90,100)
+
+
+
+
+}
