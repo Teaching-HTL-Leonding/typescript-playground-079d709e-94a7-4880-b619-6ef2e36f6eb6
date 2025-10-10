7e5d11adc80d7d4b792997d339b0aeb4c429f3be
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 08:40:03 +0200

    Saved in TypeScript Playground at 2025-10-10T06:40:03.018Z

--- flowers_basics/index.ts
+++ flowers_basics/index.ts
@@ -2,14 +2,20 @@
 
 function setup() {
   angleMode(DEGREES)
-  createCanvas(1000,1000)
+  createCanvas(1000, 1000)
   background("lightblue")
 
-  strokeWeight(10)
+  strokeWeight(20)
   fill("lightblue")
-  arc(300,300,100,100,90,100)
+  arc(500, 500, 200, 250, -90, 90)
 
-
+  fill("yellow")
+  strokeWeight(0)
+  circle(500,370,65)
+  circle
+  circle
+  circle
+  circle
 
 
 }
