c4f4a23dddf4572f2a446ee5ae8067518625fde0
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 09:16:19 +0200

    Saved in TypeScript Playground at 2025-10-10T07:16:18.548Z

--- flowers_basics/index.ts
+++ flowers_basics/index.ts
@@ -1,11 +1,11 @@
 // <<< ADD CONSTANTS HERE (if you need them)
 const KREIS_L_BREITE = 200
-const KreisL_Hoehe = 370
-const KreisL_Durchmesser = 70
-const KreisR_Breite = 500
-const KreisR_Hoehe = 370
-const KreisR_Durchmesser = 80
-const KRESIS_R_GELB = 65
+const KREIS_L_HOEHE = 370
+const KREIS_L_DURCHMESSER = 70
+const KREIS_R_BREITE = 500
+const KREIS_R_HOEHE = 370
+const KREIS_R_DURCHMESSER = 80
+const KRESIS_GELB_DURCHMESSER = 65
 
 
 function setup() {
@@ -23,15 +23,15 @@ function setup() {
   //Blütenblätter rechte blume
   fill("lightgreen")
   strokeWeight(5)
-  circle(KreisR_Breite + 50, KreisR_Hoehe, KreisR_Durchmesser)
-  circle(KreisR_Breite, KreisR_Hoehe + 50, KreisR_Durchmesser)
-  circle(KreisR_Breite - 50, KreisR_Hoehe, KreisR_Durchmesser)
-  circle(KreisR_Breite, KreisR_Hoehe - 50, KreisR_Durchmesser)
+  circle(KREIS_R_BREITE + 50, KREIS_R_HOEHE, KREIS_R_DURCHMESSER)
+  circle(KREIS_R_BREITE, KREIS_R_HOEHE + 50, KREIS_R_DURCHMESSER)
+  circle(KREIS_R_BREITE - 50, KREIS_R_HOEHE, KREIS_R_DURCHMESSER)
+  circle(KREIS_R_BREITE, KREIS_R_HOEHE - 50, KREIS_R_DURCHMESSER)
 
   //Gelber kreis rechte Blume
   fill("yellow")
   strokeWeight(0)
-  circle(KreisR_Breite, KreisR_Hoehe, KRESIS_R_GELB)
+  circle(KREIS_R_BREITE, KREIS_R_HOEHE, KRESIS_GELB_DURCHMESSER)
 
   //Linke Blume
   //Blumenstängel linke Blume
@@ -43,22 +43,13 @@ function setup() {
   // Blütenblätter linke Blume
   fill("lightgreen")
   strokeWeight(0)
-  circle(KREIS_L_BREITE + 50, KreisL_Hoehe, KreisL_Durchmesser)
-  circle(KREIS_L_BREITE + 15.5, KreisL_Hoehe + 47.5, KreisL_Durchmesser)
-  circle(KREIS_L_BREITE - 44, KreisL_Hoehe + 31.9, KreisL_Durchmesser)
-  circle(KREIS_L_BREITE + 15.5, KreisL_Hoehe - 47.5, KreisL_Durchmesser)
-  circle(KREIS_L_BREITE - 44, KreisL_Hoehe - 31.9, KreisL_Durchmesser)
+  circle(KREIS_L_BREITE + 50, KREIS_L_HOEHE, KREIS_L_DURCHMESSER)
+  circle(KREIS_L_BREITE + 15.5, KREIS_L_HOEHE + 47.5, KREIS_L_DURCHMESSER)
+  circle(KREIS_L_BREITE - 44, KREIS_L_HOEHE + 31.9, KREIS_L_DURCHMESSER)
+  circle(KREIS_L_BREITE + 15.5, KREIS_L_HOEHE - 47.5, KREIS_L_DURCHMESSER)
+  circle(KREIS_L_BREITE - 44, KREIS_L_HOEHE - 31.9, KREIS_L_DURCHMESSER)
 
   //Gelber Kreis linke Blume
   fill("yellow")
-  circle(200, 370, 65)
-
-
-
-
-
-
-
-
-
+  circle(KREIS_L_BREITE, KREIS_L_HOEHE, KRESIS_GELB_DURCHMESSER)
 }
