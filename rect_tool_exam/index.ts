70eec1265f1fed3da53abceb1ace4a1413bd3220
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 08:33:47 +0100

    Saved in TypeScript Playground at 2025-11-07T07:33:47.298Z

--- rect_tool_exam/index.ts
+++ rect_tool_exam/index.ts
@@ -8,12 +8,21 @@ function setup() {
 
 // <<< Add the function `mouseMoved` with the required code here
 function mouseMoved(){
+    const gridSize = 20;
+
+    let diameterX = Math.abs((mouseX - width/2)*2)
+    diameterX = Math.round(diameterX / gridSize)*gridSize
+    let diameterY = Math.abs ((mouseY - height/2)*2)
+    diameterY = Math.round (diameterY / gridSize)*gridSize
+
     background("black");
     fill("black")
     stroke("white")
     strokeWeight(3)
-    rect(mouseX, mouseY, mouseX+200, mouseY+100)
+    rect(mouseX, mouseY, (200-mouseX)*2, (100-mouseY)*2)
 
-
+    textAlign(LEFT);
+    const area = Math.round(diameterX / 2 * diameterY / 2 * Math.PI);
+    text(`Area: ${area}`, 5, height - 10);
 
 }
