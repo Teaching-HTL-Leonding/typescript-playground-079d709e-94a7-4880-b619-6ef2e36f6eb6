65ff128a36c57b6067d075f9502d84a0df139664
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 09:42:00 +0100

    Saved in TypeScript Playground at 2025-11-07T08:42:00.127Z

--- rect_tool_exam/index.ts
+++ rect_tool_exam/index.ts
@@ -1,4 +1,3 @@
-
 function setup() {
     createCanvas(400, 200);
     background("black");
@@ -7,24 +6,44 @@ function setup() {
 }
 
 // <<< Add the function `mouseMoved` with the required code here
-function mouseMoved(){
-    const gridSize = 20;
+function mouseMoved() {
+    const gridSize = 10
+    const snap = 10
+    const markerSize = 10
 
-    let diameterX = Math.abs((mouseX - width/2)*2)
-    diameterX = Math.round(diameterX / gridSize)*gridSize
-    let diameterY = Math.abs ((mouseY - height/2)*2)
-    diameterY = Math.round (diameterY / gridSize)*gridSize
+    const x = Math.round(mouseX / snap) * snap;
+    const y = Math.round(mouseY / snap) * snap;
 
+    const höhe = Math.round((100 - y) * 2)
+    const breite = Math.round((200 - x) * 2)
+    
     background("black");
     fill("black")
     stroke("white")
     strokeWeight(3)
-    rect(diameterX, diameterY, (200-mouseX)*2, (100-mouseY)*2)
 
+    //rect(mouseX, mouseY, (200-mouseX)*2, (100-mouseY)*2)
+    stroke("red")
+    line(x, y, x, y + höhe)
+    stroke("Yellow")
+    line(x, y, x + breite, y)
+    stroke("lightblue")
+    line(x + breite, y + höhe, x + breite, y)
+    stroke("lightgreen")
+    line(x + breite, y + höhe, x, y + höhe)
+    //Dirgonele
+    stroke("white")
+    strokeWeight(1)
+    line(x, y, x + breite, y + höhe)
+    line(x, y + höhe, x + breite, y)
 
-
+    noStroke()
+    fill("white")
     textAlign(LEFT);
-    const area = Math.round(diameterX / 2 * diameterY / 2 * Math.PI);
+    const area = Math.round((höhe) * (breite))
     text(`area: ${area}`, 5, height - 10);
 
+    text(`  ${breite}`, x + (200 - x), y - 5);
+
+    text(`  ${höhe}`, x - 30, y + (100 - y));
 }
