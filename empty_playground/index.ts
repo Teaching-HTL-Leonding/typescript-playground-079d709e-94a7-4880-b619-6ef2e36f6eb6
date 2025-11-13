0e2b4eaaef0a1167a41048e3e010f24ea995a779
Rainer Stropek <rainer@timecockpit.com>
2025-11-13 13:34:54 +0100

    Saved in TypeScript Playground at 2025-11-13T12:34:53.844Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -9,15 +9,16 @@ let colorHue: number = 0
 
 function draw() {
     background("lightblue")
-    strokeWeight(2)
+    strokeWeight(1)
     stroke("black")
     fill(colorHue, 100, 100)
     colorHue = (colorHue + 1) % 360
-    rect(x, x, 100, 50)
-    fill(colorHue, 100, 100)
     circle(x, x + 50, 50)
     circle(x + 100, x + 50, 50)
-    triangle(x,x,x+10,x+10,x-50,x)
+    rect(x, x, 100, 50)
+    noStroke()
+    fill(colorHue, 100, 100)
+    triangle(x+1,x+10,x,x-30,x+101,x)
     x = x % 500
     x = x + 1
 }
