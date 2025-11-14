e3540cb919eea4077a8e0f7173bf3523b2ac2744
Rainer Stropek <rainer@timecockpit.com>
2025-11-14 08:45:41 +0100

    Saved in TypeScript Playground at 2025-11-14T07:45:40.956Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,23 +1,18 @@
 function setup() {
-    createCanvas(500, 500)
-    background("black")
-    colorMode(HSB)
+    createCanvas(400, 400)
+    background("lightblue")
 }
 
-let x: number = 0
-let colorHue: number = 0
 
 function draw() {
     background("lightblue")
-    noStroke()
-    fill(colorHue, 100, 100)
-    colorHue = (colorHue + 1) % 360
-    circle(x, x + 50, 50)
-    circle(x + 100, x + 50, 50)
-    rect(x, x, 100, 50)
-    circle(x+100, x+25, 50)
-    noStroke()
-    fill(colorHue, 100, 100)
-    x = x % 500
-    x = x + 1
+    stroke("white")
+    fill("darkgreen")
+    rect(width / 2 - 50, height / 2 - 25, 100, 50)
+
+    if (mouseX >= width / 2 - 50 && mouseX <= width / 2 + 50
+        && mouseY >= height / 2 -25 && mouseY <= height / 2 +25){
+            text("drinnen", 10, 10)
+    }
+
 }
