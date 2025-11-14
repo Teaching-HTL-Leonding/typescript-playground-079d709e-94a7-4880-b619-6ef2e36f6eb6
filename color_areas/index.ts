e3534d5efb5a71358776146993f7f6818ab1cf53
Rainer Stropek <rainer@timecockpit.com>
2025-11-14 10:04:44 +0100

    Saved in TypeScript Playground at 2025-11-14T09:04:43.727Z

--- color_areas/index.ts
+++ color_areas/index.ts
@@ -1,19 +1,29 @@
+const textAreaHeight: number = 50
+
 function setup() {
     createCanvas(400, 200);
     background("black");
     noStroke();
 
-    fill("green")
+    fill("red")
     rect(0, 0, width / 3, height)
 
-    fill("red")
+    fill("blue")
     rect(2*width/3 , 0, width / 3, height)
 
-    fill("blue")
+    fill("green")
     rect(width/3, 0, width / 3, height)
 
     fill("black")
-    rect(0, 0, width / 3, height)
+    rect(0, height-textAreaHeight, width, textAreaHeight)
+}
+
+function mouseClicked(){
+
+
+
+
+
 
 
 }
