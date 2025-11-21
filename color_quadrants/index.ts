cfe5acee5dab5e52a7bf0fa7a2ed5866bf77f646
Rainer Stropek <rainer@timecockpit.com>
2025-11-21 08:42:35 +0100

    Saved in TypeScript Playground at 2025-11-21T07:42:34.902Z

---
+++ color_quadrants/index.ts
@@ -0,0 +1,48 @@
+const textAreaHeight: number = 50
+const textAreaWidth: number = 200
+
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    noStroke();
+
+    // <<< Add your code here
+    fill("yellow")
+    rect(0, 0, width / 2, height / 2 )
+    
+    fill("green")
+    rect(width / 2, 0, width, height / 2 )
+    
+    fill("red")
+    rect(width / 2, height / 2, width, height)
+    
+    fill("blue")
+    rect(0, height / 2, width / 2, height)
+
+    fill("black")
+    rect(width / 2 - textAreaWidth / 2, height / 2 - textAreaHeight / 2 , textAreaWidth, textAreaHeight)
+}
+
+
+
+function mouseClicked(){
+    fill("black")
+    rect(width / 2 - textAreaWidth / 2, height / 2 - textAreaHeight / 2 , textAreaWidth, textAreaHeight)
+
+    noStroke()
+    fill("white")
+    textSize(30)
+    textStyle(BOLD)
+    textAlign(CENTER, CENTER)
+
+    let message: string
+
+    if (mouseX < height / 2){
+
+        if (mouseY < width / 2)
+    }
+
+
+
+
+}
