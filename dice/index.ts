d9231adbbc80e7c9762747572b68a5fefe751d43
Rainer Stropek <rainer@timecockpit.com>
2025-11-21 09:09:22 +0100

    Saved in TypeScript Playground at 2025-11-21T08:09:21.930Z

--- dice/index.ts
+++ dice/index.ts
@@ -34,5 +34,12 @@ function setup() {
     circle(3 * width / 4, height / 4, dicePointDiameter)
     circle(width / 4, 3 * height / 4, dicePointDiameter)
     circle(width / 2, height / 2, dicePointDiameter)
-  }
+  } else if (dice === 6) {
+    circle(width / 4, height / 4, dicePointDiameter)
+    circle(3 * width / 4, height / 4, dicePointDiameter)
+    circle(3 * width / 4, 3 * height / 4, dicePointDiameter)
+    circle(width / 4, 3 * height / 4, dicePointDiameter)
+    circle(width / 4, height / 2, dicePointDiameter)
+    circle(3 * width / 4, height / 2, dicePointDiameter)
+}
 }
