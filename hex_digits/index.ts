58bff6e845d4f5a026e6de715153c17cc619f047
Rainer Stropek <rainer@timecockpit.com>
2025-11-28 10:20:52 +0100

    Saved in TypeScript Playground at 2025-11-28T09:20:51.940Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -7,8 +7,56 @@ function setup() {
   currentBase *= base
   const number3 = floor(num / currentBase) % base
   currentBase *= base
-
-
+  let digit1: string;
+  if (number1 === 10) {
+    digit1 = 'A'
+  } else if (number1 === 11) {
+    digit1 = 'B'
+  } else if (number1 === 12) {
+    digit1 = 'C'
+  } else if (number1 === 13) {
+    digit1 = 'D'
+  } else if (number1 === 14) {
+    digit1 = 'E'
+  } else if (number1 === 15) {
+    digit1 = 'F'
+  } else {
+    digit1 = `${number1}`
+  }
+    
+  let digit2: string;
+  if (number2 === 10) {
+    digit2 = 'A'
+  } else if (number2 === 11) {
+    digit2 = 'B'
+  } else if (number2 === 12) {
+    digit2 = 'C'
+  } else if (number2 === 13) {
+    digit2 = 'D'
+  } else if (number2 === 14) {
+    digit2 = 'E'
+  } else if (number2 === 15) {
+    digit2 = 'F'
+  } else {
+    digit2 = `${number2}`
+  }
+  
+  let digit3: string;
+  if (number3 === 10) {
+    digit3 = 'A'
+  } else if (number3 === 11) {
+    digit3 = 'B'
+  } else if (number3 === 12) {
+    digit3 = 'C'
+  } else if (number3 === 13) {
+    digit3 = 'D'
+  } else if (number3 === 14) {
+    digit3 = 'E'
+  } else if (number3 === 15) {
+    digit3 = 'F'
+  } else {
+    digit3 = `${number3}`
+  }
   createCanvas(300, 400)
   background("black")
 
@@ -26,7 +74,7 @@ function setup() {
 
   textSize(40)
 
-  text(number2, 80 * 0 + 75, 140)
-  text(number2, 80 * 1 + 75, 140)
-  text(number1, 80 * 2 + 75, 140)
+  text(digit3, 80 * 0 + 75, 140)
+  text(digit2, 80 * 1 + 75, 140)
+  text(digit1, 80 * 2 + 75, 140)
 }
