eb4221b2612c466450b684125073ee8e977759e7
Rainer Stropek <rainer@timecockpit.com>
2025-12-12 10:28:32 +0100

    Saved in TypeScript Playground at 2025-12-12T09:28:31.575Z

--- colored_rays/index.ts
+++ colored_rays/index.ts
@@ -6,19 +6,34 @@ function setup() {
   background("black");
   colorMode(HSB);
 }
-
 let color_hue = 0;
+
+function draw() {
+  color_hue = (color_hue + 1) % 360
+  stroke(color_hue, 100, 100)
+  background("black");
+  strokeWeight(2);
+
+  for (let a = 25; a < SIZE; a += 25) {
+
+    line(a, MARGIN, mouseX, mouseY)
+    line(a, SIZE - 25, mouseX, mouseY)
+    line(SIZE - MARGIN, a, mouseX, mouseY)
+    line(MARGIN, a, mouseX, mouseY)
+
+  }
+}
 
 function mouseMoved() {
   background("black");
   strokeWeight(2);
 
   for (let a = 25; a < SIZE; a += 25) {
-    stroke(color_hue, 100, 100)
+
     line(a, MARGIN, mouseX, mouseY)
     line(a, SIZE - 25, mouseX, mouseY)
     line(SIZE - MARGIN, a, mouseX, mouseY)
     line(MARGIN, a, mouseX, mouseY)
-    color_hue = (color_hue + 0.1) % 360
+
   }
 }
