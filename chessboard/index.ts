24be4158d86f9be2c8313116b5c27dc549b4c15e
Rainer Stropek <rainer@timecockpit.com>
2025-12-19 10:35:06 +0100

    Saved in TypeScript Playground at 2025-12-19T09:35:06.036Z

---
+++ chessboard/index.ts
@@ -0,0 +1,30 @@
+function setup() {
+    const SIZE = 500; // Canvas size
+    const SQUARE_SIZE = 50; // Size of a chess board square
+    const RAND = SQUARE_SIZE - SIZE
+
+    createCanvas(SIZE, SIZE);
+    background("black");
+
+    noStroke();
+    for (let b = 0; b < 4; b++) {
+        for (let a = 8; a > 0; a--) {
+            if (a % 2 === 1) {
+                fill("white")
+            } else {
+                fill("brown")
+            }
+            rect(a * SQUARE_SIZE, (b * 2) * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+        }
+    }
+    for (let b = 0; b < 4; b++) {
+        for (let a = 8; a > 0; a--) {
+            if (a % 2 === 1) {
+                fill("brown")
+            } else {
+                fill("white")
+            }
+            rect(a * SQUARE_SIZE, (b * 2) * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+        }
+    }
+}
