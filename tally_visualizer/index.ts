ad0057a418f7130edb8039f4ec9fab520592569b
Rainer Stropek <rainer@timecockpit.com>
2026-03-06 09:44:02 +0100

    Saved in TypeScript Playground at 2026-03-06T08:44:01.634Z

--- tally_visualizer/index.ts
+++ tally_visualizer/index.ts
@@ -17,34 +17,31 @@ const CELL_H = GROUP_H + ROW_GAP;
 // Colors
 const COLOR_FILLED = "steelblue"
 
+
 // The random number to visualize (0–500)
 let randomNumber: number;
 
 function drawLine(randomNumber: number): void {
-    console.log(randomNumber)
     fill(COLOR_FILLED)
     strokeWeight(3)
     stroke(COLOR_FILLED)
-    if (randomNumber === 1) {
-        line(MARGIN + COL_GAP / 2, TITLE_HEIGHT, MARGIN + COL_GAP / 2, TITLE_HEIGHT + GROUP_H * 3)
-    } else if (randomNumber === 2) {
-        line(MARGIN + COL_GAP / 2, TITLE_HEIGHT, MARGIN + COL_GAP / 2, TITLE_HEIGHT + GROUP_H * 3)
-        line(MARGIN + COL_GAP / 2 + COL_GAP, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP, TITLE_HEIGHT + GROUP_H * 3)
-    } else if (randomNumber === 3) {
-        line(MARGIN + COL_GAP / 2, TITLE_HEIGHT, MARGIN + COL_GAP / 2, TITLE_HEIGHT + GROUP_H * 3)
-        line(MARGIN + COL_GAP / 2 + COL_GAP, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP, TITLE_HEIGHT + GROUP_H * 3)
-        line(MARGIN + COL_GAP / 2 + COL_GAP * 2, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP * 2, TITLE_HEIGHT + GROUP_H * 3)
-    } else if (randomNumber === 4) {
-        line(MARGIN + COL_GAP / 2, TITLE_HEIGHT, MARGIN + COL_GAP / 2, TITLE_HEIGHT + GROUP_H * 3)
-        line(MARGIN + COL_GAP / 2 + COL_GAP, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP, TITLE_HEIGHT + GROUP_H * 3)
-        line(MARGIN + COL_GAP / 2 + COL_GAP * 2, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP * 2, TITLE_HEIGHT + GROUP_H * 3)
-        line(MARGIN + COL_GAP / 2 + COL_GAP * 3, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP * 3, TITLE_HEIGHT + GROUP_H * 3)
-    } else if (randomNumber === 5) {
-        line(MARGIN + COL_GAP / 2, TITLE_HEIGHT, MARGIN + COL_GAP / 2, TITLE_HEIGHT + GROUP_H * 3)
-        line(MARGIN + COL_GAP / 2 + COL_GAP, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP, TITLE_HEIGHT + GROUP_H * 3)
-        line(MARGIN + COL_GAP / 2 + COL_GAP * 2, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP * 2, TITLE_HEIGHT + GROUP_H * 3)
-        line(MARGIN + COL_GAP / 2 + COL_GAP * 3, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP * 3, TITLE_HEIGHT + GROUP_H * 3)
-        line(MARGIN + COL_GAP / 2 - COL_GAP, GROUP_H * 4, MARGIN + COL_GAP / 2 + COL_GAP * 4, TITLE_HEIGHT)
+
+    if (randomNumber > 0) {
+        if (randomNumber >= 1) {
+            line(MARGIN + COL_GAP / 2, TITLE_HEIGHT, MARGIN + COL_GAP / 2, TITLE_HEIGHT + GROUP_H * 3)
+        }
+        if (randomNumber >= 2) {
+            line(MARGIN + COL_GAP / 2 + COL_GAP, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP, TITLE_HEIGHT + GROUP_H * 3)
+        }
+        if (randomNumber >= 3) {
+            line(MARGIN + COL_GAP / 2 + COL_GAP * 2, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP * 2, TITLE_HEIGHT + GROUP_H * 3)
+        }
+        if (randomNumber >= 4) {
+            line(MARGIN + COL_GAP / 2 + COL_GAP * 3, TITLE_HEIGHT, MARGIN + COL_GAP / 2 + COL_GAP * 3, TITLE_HEIGHT + GROUP_H * 3)
+        }
+        if (randomNumber >= 5) {
+            line(MARGIN + COL_GAP / 2 - COL_GAP, TITLE_HEIGHT + GROUP_H * 3, MARGIN + COL_GAP / 2 + COL_GAP * 4, TITLE_HEIGHT)
+        }
     }
 }
 
@@ -57,7 +54,8 @@ function setup(): void {
 
     background("white");
 
-    text(randomNumber, width / 2, TITLE_HEIGHT)
+    text("Random number :    / 5", width / 2 - 60, TITLE_HEIGHT)
+    text(randomNumber, width / 2 + 40, TITLE_HEIGHT)
 
     translate(MARGIN + COL_GAP / 2, TITLE_HEIGHT + COL_GAP / 2);
     for (let row = 0; row < 1; row++) {
