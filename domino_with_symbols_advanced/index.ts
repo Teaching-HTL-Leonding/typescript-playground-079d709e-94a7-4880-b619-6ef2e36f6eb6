1b36d8752d562c1230ef992f9938261d52981e9a
Rainer Stropek <rainer@timecockpit.com>
2026-03-20 10:16:50 +0100

    Saved in TypeScript Playground at 2026-03-20T09:16:50.048Z

--- domino_with_symbols_advanced/index.ts
+++ domino_with_symbols_advanced/index.ts
@@ -1,67 +1,66 @@
 const diceSize = 200; // size of the square (width and height)
-
 function setup() {
-  createCanvas(1000, 1000);
+    createCanvas(10000, 10000);
 
+    const symbol = floor(random(1, 7));
+    const symbol2 = floor(random(1, 7))
+    const symbol3 = floor(random(1, 7))
 
-  const symbol = floor(random(1, 7));
-  const symboltwo = floor(random(1, 7))
-  const symbolthree = floor(random(1, 7))
+    for (let i = 0; i <= 600; i += 200) {
+        translate(i, 0)
+        drawDomino(symbol, symbol2)
+    }
+}
 
-  fill("lightgrey")
-  noStroke();
-  rect(0, 0, diceSize * 4, diceSize)
+function drawDomino(symbol: number, symbol2: number): void {
+    fill("lightgrey")
+    noStroke();
+    rect(0, 0, diceSize * 2, diceSize)
 
-  fill("black");
-  rect(10, 10, diceSize * 2 - 20, diceSize - 20);
+    fill("black");
+    rect(10, 10, diceSize * 2 - 20, diceSize - 20);
 
-  strokeWeight(3)
-  stroke("white")
-  line(diceSize, 20, diceSize, 180)
+    strokeWeight(3)
+    stroke("white")
+    line(diceSize, 20, diceSize, 180)
 
-  drawSymbol(symbol)
-  
-  translate(diceSize, 0)
-  drawSymbol(symboltwo)
+    drawSymbol(symbol)
 
-  translate(diceSize, 0)
-  drawSymbol(symboltwo)
-
-  translate(diceSize, 0)
-  drawSymbol(symbolthree)
+    translate(diceSize, 0)
+    drawSymbol(symbol2)
 }
 
 function drawSymbol(symbol: number): void {
-  const centerX = diceSize / 2;
-  const centerY = diceSize / 2;
-  const size = 80;
+    const centerX = diceSize / 2;
+    const centerY = diceSize / 2;
+    const size = 80;
 
-  // draw symbol
-  fill("white")
-  switch (symbol) {
-    case 1:
-      rect(centerX - 40, centerY - 40, 80, 80)
-      break
-    case 2:
-      circle(centerX, centerY, size)
-      break
-    case 3:
-      triangle(centerX, centerY - 40, centerX - 40, centerY + 40, centerX + 40, centerY + 40)
-      break
-    case 4:
-      textAlign(CENTER, CENTER)
-      textSize(size)
-      text("⭐", centerX, centerY)
-      break
-    case 5:
-      textAlign(CENTER, CENTER)
-      textSize(size)
-      text("❤️", centerX, centerY)
-      break
-    case 6:
-      textAlign(CENTER, CENTER)
-      textSize(size)
-      text("🍀", centerX, centerY)
-      break
-  }
+    // draw symbol
+    fill("white")
+    switch (symbol) {
+        case 1:
+            rect(centerX - 40, centerY - 40, 80, 80)
+            break
+        case 2:
+            circle(centerX, centerY, size)
+            break
+        case 3:
+            triangle(centerX, centerY - 40, centerX - 40, centerY + 40, centerX + 40, centerY + 40)
+            break
+        case 4:
+            textAlign(CENTER, CENTER)
+            textSize(size)
+            text("⭐", centerX, centerY)
+            break
+        case 5:
+            textAlign(CENTER, CENTER)
+            textSize(size)
+            text("❤️", centerX, centerY)
+            break
+        case 6:
+            textAlign(CENTER, CENTER)
+            textSize(size)
+            text("🍀", centerX, centerY)
+            break
+    }
 }
